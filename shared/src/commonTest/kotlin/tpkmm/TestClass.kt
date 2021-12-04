45f86415bc809bd041f52097d5f597a673a8867a
mariano <marianoledesma@alumno.unlam.edu.ar>
2021-12-04 12:26:29 -0300

    add a new test

--- shared/src/commonTest/kotlin/tpkmm/TestClass.kt
+++ shared/src/commonTest/kotlin/tpkmm/TestClass.kt
@@ -1,6 +1,8 @@
 package tpkmm
 
 
+import com.example.tp_kmm.testeoQueResteBien
+import com.example.tp_kmm.testeoQueSumeBien
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,6 +11,16 @@ class TestClass {
     @Test
     private fun testQueSumeBien(){
         val i: Int = 4
-        assertEquals(2,i)
+
+        assertEquals(testeoQueSumeBien(),i)
+        //assertEquals(2,i)
+    }
+
+    @Test
+    private fun testQueResteBien(){
+        val i = 9
+
+        assertEquals(testeoQueResteBien(),i)
+        //assertEquals(10,i)
     }
 }
